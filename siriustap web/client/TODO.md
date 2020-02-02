bd1f4dd7e48c2630017e973de03013671753dd63
GuerrillaCodester <guerrillacodester@gmail.com>
2020-02-02 20:41:50 +0000

    Make test request to server

--- siriustap web/client/TODO.md
+++ siriustap web/client/TODO.md
@@ -1,9 +1,12 @@
-# Client TODO List
+# Implement Client Boilerplate
 
-- [x] touch Index.html
-- [x] bower install bootswatch.
-- [x] Add bootswatch template css
-- [x] Add navbar
-- [x] Add jumbotron.
-- [x] touch index.js
-- [x] Add debug support.
+- [x] ClientBoilerplate template
+  - [x] touch Index.html
+  - [x] bower install bootswatch.
+  - [x] Add bootswatch template css
+  - [x] Add navbar
+  - [x] Add jumbotron.
+  - [x] touch index.js
+  - [x] Add debug support.
+- [ ] Make test request to server
+  - [ ] Fix CORS error by adding to API server-side.
--- siriustap web/server/app.js
+++ siriustap web/server/app.js
@@ -1,16 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
+const cors = require('cors');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
